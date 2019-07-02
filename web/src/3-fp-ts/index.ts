091c8f44bf28cb675109105de7410a9a04f86e92
rucka <rucka@tiscalinet.it>
2019-07-02 19:43:08 +0200

    porting to fp-ts 2

--- web/src/3-fp-ts/index.ts
+++ web/src/3-fp-ts/index.ts
@@ -1,56 +1,44 @@
 import { emailREx, urlREx, vatcodeREx } from '../_stuff/regex-collection'
 import { valOf, checkedOf } from '../_stuff/dom'
-import {
-  Validation,
-  success,
-  failure,
-  getApplicative
-} from 'fp-ts/lib/Validation'
-import { liftA5 } from './liftA5'
-import { NonEmptyArray, getSemigroup } from 'fp-ts/lib/NonEmptyArray'
-const A = getApplicative(getSemigroup<string>())
+import { NonEmptyArray, of, getSemigroup } from 'fp-ts/lib/NonEmptyArray'
+import * as _ from 'fp-ts/lib/Either'
+import { sequenceT } from 'fp-ts/lib/Apply'
+import { pipe } from 'fp-ts/lib/pipeable'
+import { identity } from 'fp-ts/lib/function'
 
-function validateName(name: string): Validation<NonEmptyArray<string>, string> {
-  if (name === '') return failure(new NonEmptyArray('Name cannot be empty', []))
+type Errors = NonEmptyArray<string>
+type Validation<A> = _.Either<Errors, A>
+const success = <A>(a: A) => _.right<Errors, A>(a)
+const failure = <A>(e: Errors) => _.left<Errors, A>(e)
+
+function validateName(name: string): Validation<string> {
+  if (name === '') return failure(of('Name cannot be empty'))
   return name.length > 3
     ? success(name)
     : failure(
-        new NonEmptyArray(
-          `Name must be at least 3 chars long, actual is ${name.length}`,
-          []
-        )
+        of(`Name must be at least 3 chars long, actual is ${name.length}`)
       )
 }
-function validateEmail(
-  email: string
-): Validation<NonEmptyArray<string>, string> {
-  if (email === '')
-    return failure(new NonEmptyArray('Email cannot be empty', []))
+function validateEmail(email: string): Validation<string> {
+  if (email === '') return failure(of('Email cannot be empty'))
   return emailREx.test(email)
     ? success(email)
-    : failure(new NonEmptyArray(`Email address '${email}' is not valid`, []))
+    : failure(of(`Email address '${email}' is not valid`))
 }
-function validateVatCode(
-  vatcode: string
-): Validation<NonEmptyArray<string>, string> {
-  if (vatcode === '')
-    return failure(new NonEmptyArray('VatCode cannot be empty', []))
+function validateVatCode(vatcode: string): Validation<string> {
+  if (vatcode === '') return failure(of('VatCode cannot be empty'))
   return vatcodeREx.test(vatcode)
     ? success(vatcode)
-    : failure(new NonEmptyArray(`VAT code '${vatcode}' is not valid`, []))
+    : failure(of(`VAT code '${vatcode}' is not valid`))
 }
-function validateUrl(url: string): Validation<NonEmptyArray<string>, string> {
+function validateUrl(url: string): Validation<string> {
   console.log(url)
   return url === null || url === '' || urlREx.test(url)
     ? success(url)
-    : failure(new NonEmptyArray(`Url '${url}' is not valid`, []))
+    : failure(of(`Url '${url}' is not valid`))
 }
-function validateAccept(
-  accept: boolean
-): Validation<NonEmptyArray<string>, boolean> {
-  return accept
-    ? success(true)
-    : failure(new NonEmptyArray(`Terms must be accepted`, []))
+function validateAccept(accept: boolean): Validation<boolean> {
+  return accept ? success(true) : failure(of(`Terms must be accepted`))
 }
 
 type Customer = {
@@ -60,51 +48,48 @@ type Customer = {
   website: string
   accept: boolean
 }
-const makeCustomer = (name: string) => (email: string) => (vatcode: string) => (
-  website: string
-) => (accept: boolean) => {
-  return {
-    name,
-    email,
-    vatcode,
-    website,
-    accept
-  } as Customer
-}
 
+const buildCustomer = ([name, email, vatcode, website, accept]: [
+  string,
+  string,
+  string,
+  string,
+  boolean
+]) => ({ name, email, vatcode, website, accept })
+
+const sequenceValidation = sequenceT(_.getValidation(getSemigroup<string>()))
 export const processCustomer = () => {
   //https://ema.codiceplastico.com/2018/08/02/fp-validation.html <- "Fp-ts validation spiegata bene"
+  //https://dev.to/gcanti/getting-started-with-fp-ts-either-vs-validation-5eja <- Getting started with fp-ts: Either vs Validation
 
-  const nameValidated: Validation<NonEmptyArray<string>, string> = validateName(
-    valOf('Name')
+  const nameValidated: Validation<string> = validateName(valOf('Name'))
+  const emailValidated: Validation<string> = validateEmail(valOf('Email'))
+  const vatcodeValidated: Validation<string> = validateVatCode(valOf('VATCode'))
+  const urlValidated: Validation<string> = validateUrl(valOf('Website'))
+  const acceptValidated: Validation<boolean> = validateAccept(
+    checkedOf('Accept')
   )
-  const emailValidated: Validation<
-    NonEmptyArray<string>,
-    string
-  > = validateEmail(valOf('Email'))
-
-  const vatcodeValidated: Validation<
-    NonEmptyArray<string>,
-    string
-  > = validateVatCode(valOf('VATCode'))
-
-  const urlValidated: Validation<NonEmptyArray<string>, string> = validateUrl(
-    valOf('Website')
+  const customerValidated = pipe(
+    sequenceValidation(
+      nameValidated,
+      emailValidated,
+      vatcodeValidated,
+      urlValidated,
+      acceptValidated
+    ),
+    _.map(buildCustomer)
   )
-  const acceptValidated: Validation<
-    NonEmptyArray<string>,
-    boolean
-  > = validateAccept(checkedOf('Accept'))
-  const liftACustomer = liftA5(A)(makeCustomer)
-  const customerValidated = liftACustomer(nameValidated)(emailValidated)(
-    vatcodeValidated
-  )(urlValidated)(acceptValidated)
 
-  const text = customerValidated.map(c => `${c.name}(${c.email})`)
-  text.map(t => alert(`I'm ${t}!`))
+  pipe(
+    customerValidated,
+    _.map<Customer, string>(c => `${c.name}(${c.email})`),
+    _.fold(
+      () => console.error('opss... something goes wrong!'),
+      t => alert(`I'm ${t}!`)
+    )
+  )
 
-  return customerValidated.fold<string[] | boolean>(
-    e => e.toArray(),
-    _ => true
-  ) as (true | string[])
+  return _.fold<Errors, Customer, true | string[]>(identity, () => true)(
+    customerValidated
+  )
 }
